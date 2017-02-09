f9b0e2a37784901144926f88a2408ac3954ee133
styyle14 <styyle14@gmail.com>
2017-02-08 23:25:18 -0600

    tweak synapse_interactive interaction

--- docker-compose_synapse_interactive.sh
+++ docker-compose_synapse_interactive.sh
@@ -7,10 +7,10 @@ THIS_SCRIPT="$(readlink -f "$0")"
 BASE_DIR="$(dirname "$THIS_SCRIPT")"
 
 DATA_DIR="${BASE_DIR}/docker-compose/docker-synapse_docker-compose"
-YAML_FILE="${DATA_DIR}/synapse.yaml"
+YAML_FILE="${DATA_DIR}/synapse_interactive.yaml"
 
 "${BASE_DIR}/scripts/docker-compose_bash-scripts/docker_run_docker-compose.sh" \
 	"$DATA_DIR" \
 	"$YAML_FILE" \
-	run --rm synapse /bin/bash
+	run --rm synapse_interactive
 
