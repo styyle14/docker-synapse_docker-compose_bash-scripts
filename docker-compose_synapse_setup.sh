d8fa9151f6fc7c67dcb264f436ef0357841c8637
styyle14 <styyle14@gmail.com>
2017-02-08 22:44:08 -0600

    fix docker-synapse_setup

--- docker-compose_synapse_setup.sh
+++ docker-compose_synapse_setup.sh
@@ -6,11 +6,11 @@ set -e
 THIS_SCRIPT="$(readlink -f "$0")"
 BASE_DIR="$(dirname "$THIS_SCRIPT")"
 
-DATA_DIR="${BASE_DIR}/docker-compose/docker-bitcoin_docker-compose"
-YAML_FILE="${DATA_DIR}/bitcoin-qt.yaml"
+DATA_DIR="${BASE_DIR}/docker-compose/docker-synapse_docker-compose"
+YAML_FILE="${DATA_DIR}/synapse_setup.yaml"
 
 "${BASE_DIR}/scripts/docker-compose_bash-scripts/docker_run_docker-compose.sh" \
 	"$DATA_DIR" \
 	"$YAML_FILE" \
-	run --rm bitcoin-qt "${@:1}"
+	run --rm synapse_setup
 
